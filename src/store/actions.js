09452f34cc45bbc624239d21d1cce8f3f0f29794
chris <dwatow@gmail.com>
2018-05-21 10:37:51 +0800

    delete login api

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,22 +1,5 @@
 import axios from 'axios'
 export default {
-  async Login ({ dispatch, commit, getters }) {
-    const headers = {
-      'Content-Type': 'application/json',
-    }
-    const data = {
-      'grant_type': 'password',
-      'username': 'root@email.com', // ${payload.username}
-      'password': 'password', // ${payload.passworld}
-      'client_id': '2',
-      'client_secret': 'TCqXPHubxIcPHCCAA1IwyZKMx3txKMWVsvC0oFmV',
-    }
-
-    const response = await axios.post(`${getters.host}/oauth/token`, data, { headers })
-    commit('token', response.data)
-    await dispatch('whoAmI')
-    Promise.resolve()
-  },
   async LoginQRcode ({ dispatch, commit, getters }) {
     const headers = {
       'Content-Type': 'application/json',
