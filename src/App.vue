61e8d32ffb1aa0904684222d1a39cc0d6d4d359b
chris <dwatow@gmail.com>
2018-05-29 10:57:29 +0800

    add custom-theme, fix allChildAccount

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,12 @@ export default {
 <style>
 #app {
   background:
-  url("../static/images/gems_decoration_L.png") no-repeat left 110%,
-  url("../static/images/gems_decoration_R.png") no-repeat right bottom,
+  url("../static/images/gems_decoration_L.png") no-repeat left 118%,
+  url("../static/images/gems_decoration_R.png") no-repeat right 120%,
   url("../static/images/bg_lighting.png") no-repeat center bottom;
   background-size: auto, auto, cover;
+  background-attachment: fixed, fixed, scroll;
   color: #f8f8f9;
 }
 
-.ivu-form .ivu-form-item-label {
-  color: #f8f8f9;
-}
 </style>
--- src/iview/index.js
+++ src/iview/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import iview from 'iview'
 import 'iview/dist/styles/iview.css'
+import './custom-theme.css'
 
 Vue.use(iview)
