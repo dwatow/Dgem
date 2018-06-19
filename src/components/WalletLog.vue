a7667bc037a589193e9ffb56301ae387959d7810
chris <dwatow@gmail.com>
2018-06-19 19:59:03 +0800

    fix without user in walletlog

--- src/components/WalletLog.vue
+++ src/components/WalletLog.vue
@@ -33,8 +33,8 @@ export default {
           minWidth: 150,
         },
         {
-          title: '操作者 ID',
-          key: 'operator_id',
+          title: '操作者䁥稱',
+          key: 'operator_name',
           minWidth: 100,
         },
         {
@@ -60,8 +60,8 @@ export default {
           width: 150,
         },
         {
-          title: '接受人 ID',
-          key: 'user_id',
+          title: '接受人䁥稱',
+          key: 'user_name',
           minWidth: 100,
         },
         // {
@@ -79,6 +79,8 @@ export default {
         item.item = `${this.$store.getters.gems[item.result_data.gem]}`
         item.amount = item.result_data.amount
         item.delta_amount = (item.delta !== null) ? `${item.delta.amount}` : '0'
+        item.operator_name = item.operator ? item.operator.name : ''
+        item.user_name = item.user ? item.user.name : ''
         return item
       })
     },
